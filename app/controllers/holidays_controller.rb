cedca3794b920441e4ca5ace1cc431c6cd1e5e44
Matthias Kutz <matthias.kutz@gmx.net>
2021-05-25 18:51:28 +0200

    added cloudinary so that users can upload pictures

--- app/controllers/holidays_controller.rb
+++ app/controllers/holidays_controller.rb
@@ -38,6 +38,6 @@ class HolidaysController < ApplicationController
   end
 
   def holiday_params
-    params.require(:holiday).permit(:city, :country, :description, :rating)
+    params.require(:holiday).permit(:city, :country, :description, :rating, :photo)
   end
 end
--- app/models/holiday.rb
+++ app/models/holiday.rb
@@ -1,2 +1,3 @@
 class Holiday < ApplicationRecord
+  has_one_attached :photo
 end
