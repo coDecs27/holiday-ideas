8fdf07875b4ac22a4a310d9870aed6a2c4071522
Matthias Kutz <matthias.kutz@gmx.net>
2021-05-25 11:16:40 +0200

    added seeds and removed holiday.name from index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Holiday.create(
+    city: "Kreta",
+    country: "Griechenland",
+    description: "Das Hinterland ist hügelig bis bergig und wer Lust hat Land & Leute kennen zu lernen, kann auf eigene Faust die reizvolle Umgebung oder das 25 km entfernte Heraklion erkunden.",
+    rating: 5
+  )
+
+  Holiday.create(
+    city: "Majjistral",
+    country: "Malta",
+    description: "Entdecken Sie die Wärme der traditionellen maltesischen Gastfreundlichkeit, die Aromen autentischer Kochkunst und die Schönheit des Mittelmeers- Erleben Sie den Urlaub, den Sie verdient haben.",
+    rating: 4
+  )
+
+  Holiday.create(
+    city: "Tirol",
+    country: "Österreich",
+    description: "Tirol ist ein Bundesland im Westen Österreichs, das in den Alpen liegt und für seine Skigebiete, historischen Stätten und Volksbräuche bekannt ist.",
+    rating: 3
+  )
+
+  Holiday.create(
+    city: "Agia Napa",
+    country: "Zypern",
+    description: "Ayia Napa ist ein Ferienort mit bekannten Stränden an der Südküste der Mittelmeerinsel Zypern. Die wichtigste Sehenswürdigkeit der Stadt ist das historische Kloster aus der Zeit der venezianischen Kreuzfahrer, das auf dem Platz Plateia Seferi in der Altstadt steht und von Bars und Clubs umgeben ist.",
+    rating: 4
+  )
+
+  Holiday.create(
+    city: "Giardini Naxos",
+    country: "Italien",
+    description: "Giardini-Naxos oder kurz Giardini ist eine italienische Gemeinde der Metropolitanstadt Messina auf der Mittelmeerinsel Sizilien mit knapp 10.000 Einwohnern.",
+    rating: 5
+  )
+
+  Holiday.create(
+    city: "Kolberg",
+    country: "Polen",
+    description: "Kolberg ist ein polnisches Ostseebad, das für seine Solequellen bekannt ist. Die gotische Marienbasilika besitzt ein aus dem 14. Jahrhundert stammendes Taufbecken und einen siebenarmigen Leuchter aus dem 16. Jahrhundert.",
+    rating: 4
+  )
+  
